d3b851b6bb6528d9618784fbb97cdd08b004f40f
Mykola Honcharenko <nikolay.honcharenko#gmail.com>
2026-06-14 19:45:22 +0300

    Solution.

--- task.sql
+++ task.sql
@@ -1,3 +1,14 @@
 USE ShopDB; 
-
--- Create your stored procedure here
+-- Він повинен повернути таблицю зі списком назв продуктів, а також їх кількістю на складі.
+DELIMITER //
+CREATE PROCEDURE get_warehouse_product_inventory(
+    IN warehouse_id INT
+)
+BEGIN
+    SELECT Name,
+           WarehouseAmount
+    FROM Products
+        INNER JOIN ProductInventory
+            ON WarehouseID = warehouse_id;
+END //
+DELIMITER ;
