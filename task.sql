b35afce563a4e8c44bd7bf87b232bb10858e9e0f
Mykola Honcharenko <nikolay.honcharenko#gmail.com>
2026-06-15 01:29:30 +0300

    Зміна на конструкцію WHERE.

--- task.sql
+++ task.sql
@@ -6,8 +6,8 @@ BEGIN
     SELECT Products.Name,
            ProductInventory.WarehouseAmount
     FROM Products
-        INNER JOIN ProductInventory
-            ON ProductInventory.ProductID = Products.ID
-            AND ProductInventory.WarehouseID = warehouse_id;
+    INNER JOIN ProductInventory ON ProductInventory.ProductID = Products.ID
+    WHERE ProductInventory.WarehouseID = warehouse_id;
+
 END //
 DELIMITER ;
